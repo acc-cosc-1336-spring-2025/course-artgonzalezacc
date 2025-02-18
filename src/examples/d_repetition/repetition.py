4a678365787eb19899ee896c8fab71dbdd5a509f
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-02-18 01:45:47 +0000

    while sum of squares

--- src/examples/d_repetition/repetition.py
+++ src/examples/d_repetition/repetition.py
@@ -12,3 +12,17 @@ def use_a_while_loop(num):
         counter = counter + 1
         if(counter == 3):
             print(counter, counter < num,  '')
+
+#3   1*1 + 2*2 + 3*3 = 14
+#4    1*1 + 2*2 + 3*3 + 4*4 = 30
+#5    1*1 + 2*2 + 3*3 + 4*4  + 5*5= 55
+def get_sum_of_squares(num): #4
+     
+    sum = 0
+
+    while(num > 0):
+        sum = sum + num * num
+        num = num - 1 #will make num > 0 False
+         
+    return sum
+         
