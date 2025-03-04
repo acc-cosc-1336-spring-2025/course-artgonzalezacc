66de31049daf735fc7a25245d2d5e74d3cec1ec0
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-03-04 01:09:26 +0000

    string is immutable-can't change it

--- src/examples/h_strings/main.py
+++ src/examples/h_strings/main.py
@@ -2,6 +2,7 @@
 
 def main():
     lang = 'python'
+    #lang[0] = 'P'--generates a TypeError-we can't change a string
     print(lang[0])
     #concept of the length / size of the string
     print(len(lang))
