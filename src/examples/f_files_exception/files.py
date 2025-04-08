f0bc30cf725ba5a79e6c1f8167171d3e6484032c
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-04-08 00:50:54 +0000

    write/read records to file

--- src/examples/f_files_exception/files.py
+++ src/examples/f_files_exception/files.py
@@ -45,3 +45,36 @@ def read_from_file_w_for(file_name):
         print(line.rstrip('\n'))
 
     file.close()
+
+def write_employee_records(file_name):
+    file = open(file_name, 'w')
+
+    choice = '1'
+
+    while choice == '1':
+        id = input('Enter id: ')
+        name = input('Enter name: ')
+        dept = input('Enter dept: ')
+
+        file.write(id + '\t')
+        file.write(name + '\t')
+        file.write(dept + '\n')
+
+        choice = input('Enter 1 to continue: ')
+    
+    file.close()
+
+def read_employee_records(file_name):
+    file = open(file_name, 'r')
+
+    for employee in file:
+        record = employee.split('\t') #this creates a list ['1336', 'Python', 'COSC']
+
+        id = record[0]
+        name = record[1]
+        dept = record[2].rstrip('\n')
+
+        print(id, name, dept)
+
+    file.close()
+
--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -2,11 +2,9 @@
 import files
 
 def main():
-    file_name = 'philosophers.txt'
-    #files.write_to_file(file_name)
-    #files.read_from_file(file_name)
-    #files.read_from_file_one_line_at_a_time(file_name)
-    #files.read_from_file_w_while_loop(file_name)
-    files.read_from_file_w_for(file_name)
+    file_name = 'employees.txt'
+
+    #files.write_employee_records(file_name)
+    files.read_employee_records(file_name)
 
 main()
