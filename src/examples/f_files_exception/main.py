3ac02db8f80afae007283c213b012ad9e13ac0ee
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-04-09 20:51:44 -0500

    unpickle a dictionary

--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -3,7 +3,8 @@ import files
 
 def main():
     file_name = 'phone_book.txt'
-    files.pickle_dictionary(file_name)
+    #files.pickle_dictionary(file_name)
+    files.read_image_jpg_file(file_name)
     
 
 main()
