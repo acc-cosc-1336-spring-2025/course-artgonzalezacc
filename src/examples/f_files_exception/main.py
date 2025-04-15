9fac3b46e2ea110a8311a12b0eb038ca26e7fd8a
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-04-15 01:14:29 +0000

    divide by zero def code

--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -1,10 +1,26 @@
 #main program
-import files
+import exceptions
 
 def main():
-    file_name = 'phone_book.txt'
-    #files.pickle_dictionary(file_name)
-    files.read_image_jpg_file(file_name)
+    num2 = 0
+    num1 = 0
+    
+    while(num2 == 0 ):
+        
+        num1 = input('Enter a number: ')
+        while(not num1.isdigit()):
+            num1 = input('Enter a number(1234 etc): ')
+        
+        num2 = input("Enter divisor(must be gt 0): ")
+        while(not num2.isdigit()):
+            num2 = input("Enter divisor(must be gt 0) (1234 etc): ")
+
+        num2 = int(num2)
+
+    exceptions.divide_by_zero(int(num1), int(num2))
+
+    exceptions.divide_by_zero(5, 0)
+
     
 
 main()
