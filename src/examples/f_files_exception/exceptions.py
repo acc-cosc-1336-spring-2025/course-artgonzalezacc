64ee01af6b58a0dc719a431f5d60121272f1a3ff
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-04-15 01:15:54 +0000

    handle open file w exception

--- src/examples/f_files_exception/exceptions.py
+++ src/examples/f_files_exception/exceptions.py
@@ -1,2 +1,32 @@
 def test_config():
     return True
+
+def divide_by_zero(num1, num2):
+    if(num2 == 0):
+        print("Num2 must be gt 0")    
+    else:
+        print(str(num1/num2))
+
+def open_a_file_error(file_name):
+    
+    log_file = open('system_log.dat', 'w')
+
+    try:
+        file = open(file_name, 'r')
+        
+        for line in file:
+            amount = int(line)
+            print(amount)
+
+        file.close()
+    except IOError as err:
+        print('Cannot open file: ' + file_name)
+        log_file.write("exceptions.py in open_a_file_error: " + str(err) + '\n')
+    except ValueError as err:
+        print('Invalid numeric data')
+        log_file.write("exceptions.py in open_a_file_error: " + str(err) + '\n')
+    except:
+        print('unknown error')
+
+    log_file.close()
+
--- src/examples/f_files_exception/main.py
+++ src/examples/f_files_exception/main.py
@@ -2,24 +2,8 @@
 import exceptions
 
 def main():
-    num2 = 0
-    num1 = 0
     
-    while(num2 == 0 ):
-        
-        num1 = input('Enter a number: ')
-        while(not num1.isdigit()):
-            num1 = input('Enter a number(1234 etc): ')
-        
-        num2 = input("Enter divisor(must be gt 0): ")
-        while(not num2.isdigit()):
-            num2 = input("Enter divisor(must be gt 0) (1234 etc): ")
-
-        num2 = int(num2)
-
-    exceptions.divide_by_zero(int(num1), int(num2))
-
-    exceptions.divide_by_zero(5, 0)
+    exceptions.open_a_file_error('myfile')
 
     
 
