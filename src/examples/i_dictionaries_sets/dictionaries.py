c6d9663362ad4d0fef74d3ff336d6738f5a568b1
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-04-17 00:29:25 +0000

    display survey questions/options

--- src/examples/i_dictionaries_sets/dictionaries.py
+++ src/examples/i_dictionaries_sets/dictionaries.py
@@ -1,6 +1,26 @@
 def test_config():
     return True
 
+#Create dictionaries
+survey_questions = \
+{
+    '2.1': 'The texts, assigned readings, and other course resources help me learn the course material.',
+    '2.2': 'Clear instructions are provided',
+    '2.3': 'Tests, papers, assignments, and or other activities include clear instructions.',
+    '2.4': 'Uses instructional technology',
+    '2.5': 'Tests, papers, assignments, or other activities'
+}
+
+survey_question_options = \
+{
+    1: 'Never',
+    2: 'Sometimes',
+    3: 'More',
+    4: 'More more',
+    5: 'Always',
+    6: 'Always Always'
+}
+
 def display_menu():
     print('1-Enter Survey Responses')
     print('2-Get Survey Results')
@@ -16,10 +36,19 @@ def run_menu():
 
 def handle_menu_option(option):
     if(option == 1):
-        print('display survey question')
+        display_survey_questions()
     elif(option == 2):
         print('survey results')
     elif(option == 3):
         print('Exiting...')
     else:
         print('Invalid option ...')
+
+def display_survey_questions():
+    for question_id, question in survey_questions.items():
+        print(question_id, question)
+
+        for option, value in survey_question_options.items():
+            print(option, value)
+        
+        response = input("Enter response: ")
