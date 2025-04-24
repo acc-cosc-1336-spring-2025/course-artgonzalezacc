5ae4e1f3815fd91f5bde9df36ecf35a5320bf06d
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-04-24 00:44:52 +0000

    use withdraw in main

--- src/examples/j_classes/main.py
+++ src/examples/j_classes/main.py
@@ -8,4 +8,8 @@ def main():
     account1.deposit(int(amount))
     print(account1.get_balance())
 
+    amount = input("Enter amount to withdraw: ")
+    account1.withdraw(int(amount))
+    print(account1.get_balance())
+
 main()
