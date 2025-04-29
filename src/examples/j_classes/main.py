6b86440cbb8c99f152765bbc69bb2526a14c88c3
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2025-04-29 00:24:37 +0000

    add atm class

--- src/examples/j_classes/main.py
+++ src/examples/j_classes/main.py
@@ -1,15 +1,15 @@
 from bank_account import BankAccount
+from atm import ATM
 
 def main():
     account1 = BankAccount(500) #variable represents a BankAccount--- object or instance of a class
-    print(account1.get_balance())
+    atm = ATM(account1)
 
-    amount = input("Enter amount to deposit: ")
-    account1.deposit(int(amount))
-    print(account1.get_balance())
+    atm.display_balance()
+    atm.make_deposit()
+    atm.make_withdraw()
 
-    amount = input("Enter amount to withdraw: ")
-    account1.withdraw(int(amount))
+    atm.display_balance()
     print(account1.get_balance())
 
 main()
